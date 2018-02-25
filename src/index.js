8b6b3c1db07b33ab5a00b2ace9336394a2bd32d7
Michael Genesini <michael.genesini@gmail.com>
2018-02-25 12:00:01 +0100

    added modules to testing tree shaking

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import './main.sass'
+// import './main.sass'
 
-(() => {
-    const DATA = {
-        firstName: 'Parrot',
-        lastName: 'Party'
-    }
-    let {firstName, lastName} = DATA
-    console.log(`Hello ${firstName} ${lastName}!`)
-})()
+
+// Import a couple modules for testing.
+import { sayHelloTo } from './modules/mod1'
+import addArray from './modules/mod2'
+
+const DATA = {
+    firstName: 'Parrot',
+    lastName: 'Party'
+}
+let {firstName, lastName} = DATA
+console.log(`Hello ${firstName} ${lastName}!`)
+
+// Run some functions from our imported modules.
+const result1 = sayHelloTo('Party Parrot')
+const result2 = addArray([1, 2, 3, 4])
+
+// Print the results on the page.
+const printTarget = document.getElementById('test')
+
+printTarget.innerText = `sayHelloTo('Party') => ${result1}\n\n`
+printTarget.innerText += `addArray([1, 2, 3, 4]) => ${result2}`
